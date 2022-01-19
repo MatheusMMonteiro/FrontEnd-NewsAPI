abaa1bbd7f3d40dc9b1e81043c9712c6f15960f6
Matheus <matheus.malmeida7@gmail.com>
2022-01-18 22:03:27 -0300

    Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateNewsComponent } from './create-news/create-news.component';
+import { DeleteNewsComponent } from './delete-news/delete-news.component';
+import { ListNewsComponent } from './list-news/list-news.component';
+import { UpdateNewsComponent } from './update-news/update-news.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'list-news', pathMatch: 'full'},
+  {path: 'list-news', component: ListNewsComponent},
+  {path: 'create-news', component: CreateNewsComponent},
+  {path: 'update-news/:id', component: UpdateNewsComponent},
+  {path: 'delete-news/:id', component: DeleteNewsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ListNewsComponent } from './list-news/list-news.component';
+import { CreateNewsComponent } from './create-news/create-news.component';
+import { FormsModule } from '@angular/forms';
+import { UpdateNewsComponent } from './update-news/update-news.component';
+import { DeleteNewsComponent } from './delete-news/delete-news.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ListNewsComponent,
+    CreateNewsComponent,
+    UpdateNewsComponent,
+    DeleteNewsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
